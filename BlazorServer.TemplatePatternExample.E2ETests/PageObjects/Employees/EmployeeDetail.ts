56dfbc510eed9c5036427ab61589d6af4c0b98db
Fletcher Trueblood <ftrueblood@jgboswell.com>
2023-12-21 15:20:30 -0800

    use super to re use code from base class

--- BlazorServer.TemplatePatternExample.E2ETests/PageObjects/Employees/EmployeeDetail.ts
+++ BlazorServer.TemplatePatternExample.E2ETests/PageObjects/Employees/EmployeeDetail.ts
@@ -20,25 +20,9 @@ export class EmployeeDetail extends DetailBase {
     }
 
     public async populateForm(t: TestController, name: string): Promise<void> {
-        const active = this.getActiveSelector();
-        const nameInput = this.getNameSelector();
-        const description = this.getDescriptionSelector();
-        const department = this.getDepartmentSelector();
-
-        await t
-            .expect(active.exists).ok()
-            .expect(nameInput.exists).ok()
-            .expect(description.exists).ok()
-            .expect(department.exists).ok()
-            .expect(this.manager.exists).ok();
-
-        await t
-            .typeText(nameInput, name)
-            .typeText(department, generateString(8))
-            .typeText(description, generateString(8))
-            .click(active);
-
+        await t.expect(this.manager.exists).ok();
         await this.testHelpers.selectFromDropDown(t, this.manager, 0);
+        super.populateForm(t, name);
     }
 }
 
